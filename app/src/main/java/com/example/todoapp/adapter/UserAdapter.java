add270d2f81e39da69e5687a54a0174af3b36750
fitri alfiana <fitrialfiana62@gmail.com>
2022-06-08 18:52:29 +0700

    commit1

---
+++ app/src/main/java/com/example/todoapp/adapter/UserAdapter.java
@@ -0,0 +1,19 @@
+package com.example.todoapp.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.todoapp.R;
+import com.example.todoapp.model.User;
+
+import java.util.List;
+
+public class UserAdapter {
+
+}
