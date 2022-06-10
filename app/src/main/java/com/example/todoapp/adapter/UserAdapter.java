67f8a11a5939210f53b9a2024449db2e8e9de799
fitri alfiana <fitrialfiana62@gmail.com>
2022-06-10 16:29:25 +0700

    Update firebase

--- app/src/main/java/com/example/todoapp/adapter/UserAdapter.java
+++ app/src/main/java/com/example/todoapp/adapter/UserAdapter.java
@@ -1,5 +1,6 @@
 package com.example.todoapp.adapter;
 
+import android.app.Dialog;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,6 +15,64 @@ import com.example.todoapp.model.User;
 
 import java.util.List;
 
-public class UserAdapter {
+public class UserAdapter extends RecyclerView.Adapter<UserAdapter.MyViewHolder> {
+    private Context context;
+    private List<User> list;
+    private Dialog dialog;
 
+    public interface Dialog {
+        void onClick(int pos);
+    }
+
+    public void setDialog(Dialog dialog) {
+        this.dialog = dialog;
+    }
+
+    public UserAdapter(Context context, List<User> list) {
+        this.context = context;
+        this.list = list;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.row_user, parent, false);
+        return new MyViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        holder.name.setText(list.get(position).getName());
+        holder.email.setText(list.get(position).getEmail());
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+    class MyViewHolder extends RecyclerView.ViewHolder {
+        TextView name, email;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            name = itemView.findViewById(R.id.name);
+            email = itemView.findViewById(R.id.email);
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if (dialog != null) {
+                        dialog.onClick(getLayoutPosition());
+                    }
+                }
+            });
+        }
+    }
 }
+
+
+
+
+
+
+
